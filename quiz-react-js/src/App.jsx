1949afd0faecd6e70697327212aa54af8ebc6c4c
will-developer <william_limaa@hotmail.com>
2024-07-31 15:28:45 -0300

    feat: changing the page stage from start to question, adding Question.jsx and Question.css to the components folder. Additionally, the files were updated with scripts to handle the stage change, with modifications in Questions.jsx, Welcome.jsx, and App.jsx.

--- quiz-react-js/src/App.jsx
+++ quiz-react-js/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
+import { useContext } from 'react';
+import { QuizContext } from './context/quiz';
+
 import Welcome from './components/Welcome';
+import Question from './components/Question';
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <Welcome />
+      {quizState.gameStage === 'Start' && <Welcome />}
+      {quizState.gameStage === 'Playing' && <Question />}
     </div>
   );
 }
